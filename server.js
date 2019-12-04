85291752ef4ec5cbab6c5a9db914aa2475157df2
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-04 11:20:24 -0800

    Finish follow along of Guided project.

--- server.js
+++ server.js
@@ -1,34 +1,47 @@
 const express = require('express'); // importing a CommonJS module
 const helmet = require('helmet');
+
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
 //middleware----------------------------------------------------
+// server.use(helmet());//<<<<<<<<<<<<<<<<<<use it 2;
+server.use(express.json());//built in middleware
+server.use(logger);
+
 // custom middleware
 function logger(req, res, next) {
   console.log(`${req.method} to ${req.originalUrl}`)
-  
+
   next();//allows the request to continue to the next middleware
 }
 
-  // Write a gatekeeper middleware that reads a password
-  function atGate(req, res, next) {
-    console.log('At the Gate about to be eaten')
+// Write a gatekeeper middleware that reads a password
+function gateKeeper(req, res, next) {
+  const password = req.headers.password;
+  if (password && password.toLowerCase() === "melon") {
     next();
+  } else {
+    res.status(401).json({ you: "shall not pass!!" });
+  }
 }
 
-
-
-// server.use(helmet());//<<<<<<<<<<<<<<<<<<use it 2;
-server.use(express.json());//built in middleware
-
-server.use('/api/hubs', hubsRouter);// the router is local middleware because it only applies to the api/hubs
-
-
-server.use(logger())
-
+// checkRole('admin') , checkRole('agents')
+function checkRole(role) {
+  return function (req, res, next) {
+    if (role && role === req.headers.role) {
+      next();
+    } else {
+      res.status(403).json({ message: "can't touch this!" });
+    }
+  };
+}
 // endpoints----------------------------------------------------
+
+// the router is local middleware because it only applies to the api/hubs
+server.use("/api/hubs", checkRole("admin"), hubsRouter);
+
 server.get('/', (req, res) => {
   const nameInsert = (req.name) ? ` ${req.name}` : '';
 
@@ -38,19 +51,15 @@ server.get('/', (req, res) => {
     `);
 });
 
-server.get('/echo', (req,res) => {
+server.get('/echo', (req, res) => {
   res.send(req.headers);
 })
 
 //invoking it in the endpoint makes it locally
-server.get('/area51', atGate(),  (req,res) => {
-  // res.send(req.headers);
-  if(req.password === 'mellon') {
-res.status(200).json({message: "Come on in. The water is fine."})
-  }else {
-    res.status(401).json({message: "You shall not pass!!!"})
-  }
+server.get('/area51', gateKeeper, checkRole("agent"), (req, res) => {
+  res.send(req.headers);
 })
 
 // export-------------------------------------------------------
 module.exports = server;
+
