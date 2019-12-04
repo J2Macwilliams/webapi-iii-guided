bb73592eef06c4e623674a1d4b89015a2115573e
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-04 10:11:52 -0800

    Almost finished.

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 const express = require('express'); // importing a CommonJS module
-
+const helmet = require('helmet');
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
-server.use(express.json());
+//middleware----------------------------------------------------
+// custom middleware
+function logger(req, res, next) {
+  console.log(`${req.method} to ${req.originalUrl}`)
+  
+  next();//allows the request to continue to the next middleware
+}
 
-server.use('/api/hubs', hubsRouter);
+  // Write a gatekeeper middleware that reads a password
+  function atGate(req, res, next) {
+    console.log('At the Gate about to be eaten')
+    next();
+}
 
+
+
+// server.use(helmet());//<<<<<<<<<<<<<<<<<<use it 2;
+server.use(express.json());//built in middleware
+
+server.use('/api/hubs', hubsRouter);// the router is local middleware because it only applies to the api/hubs
+
+
+server.use(logger())
+
+// endpoints----------------------------------------------------
 server.get('/', (req, res) => {
   const nameInsert = (req.name) ? ` ${req.name}` : '';
 
@@ -17,4 +38,19 @@ server.get('/', (req, res) => {
     `);
 });
 
+server.get('/echo', (req,res) => {
+  res.send(req.headers);
+})
+
+//invoking it in the endpoint makes it locally
+server.get('/area51', atGate(),  (req,res) => {
+  // res.send(req.headers);
+  if(req.password === 'mellon') {
+res.status(200).json({message: "Come on in. The water is fine."})
+  }else {
+    res.status(401).json({message: "You shall not pass!!!"})
+  }
+})
+
+// export-------------------------------------------------------
 module.exports = server;
